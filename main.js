70428d0f818e850f565f6a2139270a243f25a9ed
rocky <www.drougnov182@gmail.com>
2023-11-07 10:29:52 +0600

    Added the local styles & script & updated main.js

--- main.js
+++ main.js
@@ -22,28 +22,44 @@ function addScriptToDOM(url, type = "absolute") {
 }
 
 const header = document.querySelector(".header");
+// store the timeout ID as variable
+let timeoutId;
+
 // Add an event listener for the custom 'popupOpened' event
 document.addEventListener("popupOpened", function (e) {
-    console.log("popup opened");
     if (e.detail && e.detail.target) {
         // Add 'nav-active' class to header if header exist and the target equals '#header-popup'
         if (header && e.detail.target === "#header-popup") {
             header.classList.add("nav-active");
         }
 
-        const popup = document.querySelector(`${e.detail.target}`);
-        const popupElementsContainer = popup.querySelector(".ExGby");
-        setTimeout(() => {
+        // Clear the previous timeout if it exists
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        const popup = document.querySelector(`${e.detail.target}`); // popup overlay (.DukSh)
+        const popupElementsContainer = popup.querySelector(".ExGby"); // popup container
+
+        // Set a new setTimeout function
+        timeoutId = setTimeout(() => {
+            // get container's visible height
             const popupElementsContainerHeight =
                 popupElementsContainer.offsetHeight;
+
+            // get container's actual (scrollable) height
             const popupElementsHeight = popupElementsContainer.scrollHeight;
+
             console.log(popupElementsContainerHeight, popupElementsHeight);
 
-            // Check if the content's height is greater than container's height
+            // Check if the content's actual height is greater than container's visible height
             if (popupElementsHeight > popupElementsContainerHeight) {
+                // if yes, add the class
                 popup.classList.add("exceeded");
             } else {
+                // check if the class already exist
                 if (popup.classList.contains("exceeded")) {
+                    // if yes, remove the class
                     popup.classList.remove("exceeded");
                 }
             }
